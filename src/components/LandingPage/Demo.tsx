4f4d3270edd1b9ba9b267e1831699430910838cf
Khan047 <tajzz.tk@gmail.com>
2022-09-07 20:29:08 +0530

    :heavy_check_mark: slider changed, del unsed code

--- src/components/LandingPage/Demo.tsx
+++ src/components/LandingPage/Demo.tsx
@@ -37,7 +37,6 @@ const BlockNFT = tw.div`
   md:h-142
 `;
 
-
 const Subtitle = tw.div`
   text-3xl
   xsm:text-3.25xl
@@ -87,13 +86,13 @@ const ImageC = styled.div`
 
   .current {
     left: 26.5%;
-    opacity:1;
+    opacity: 1;
   }
 
   .current.move {
     left: 0;
-    opacity:0;
-    transition: all 0.5s ease;
+    opacity: 0;
+    // transition: all 0.5s ease;
   }
 
   .next {
@@ -102,15 +101,15 @@ const ImageC = styled.div`
 
   .next.move {
     left: 100%;
-    transition: all 0.5s ease;
+    // transition: all 0.5s ease;
   }
   .pic-wrapper {
     background: transparent;
     position: relative;
     border-radius: 12px;
-    height:100%;
-    display:flex;
-    justify-content:center;
+    height: 100%;
+    display: flex;
+    justify-content: center;
   }
 
   .mask {
@@ -207,158 +206,71 @@ z-20
 relative
 `;
 const AnnonBack = styled.div`
-width:65%;
-background-image: url(${mainshapeBook});
-background-repeat: no-repeat;
-background-size:contain;
-background-position:center;
-height:100%;
-position:absolute;
-top:-10%;
-left:-8%;
-z-index:0;
+  width: 65%;
+  background-image: url(${mainshapeBook});
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  height: 100%;
+  position: absolute;
+  top: -10%;
+  left: -8%;
+  z-index: -10;
 
-@media only screen and (max-width: 920px) {
-  width:100%;
-  top: -20%;
-  left: -7%;
-}
-`;
-const List = styled.div`
-  &>li::marker{
-    color:#A421ED;
+  @media only screen and (max-width: 920px) {
+    width: 100%;
+    top: -20%;
+    left: -7%;
   }
 `;
-const AnimatedImage: React.FC<{
-  index: any;
-  move: any;
-  innerRef: any;
-  arrowUsed: boolean;
-}> = ({ index, move, innerRef, arrowUsed }) => {
-  useEffect(() => {}, [index, arrowUsed]);
+const List = styled.div`
+  & > li::marker {
+    color: #a421ed;
+  }
+`;
 
-  return (
-    <div className={`pic-wrapper`}>
-      <div ref={innerRef} className={`current pic ${move}`}>
-        <img
-          key={index}
-          src={content.carouselImageNames[index]}
-          alt={"asd" + index}
-        />
-      </div>
-    </div>
-  );
-};
-
-
+const Truck = styled.div`
+  img.ph {
+    width: 100%;
+    height: auto;
+    max-width: 50%;
+    display: block;
+    margin: 0 auto;
+    z-index:1;
+  }
+`;
 const Join = (): JSX.Element => {
-  const getNextIndex = (idx: number) => {
-    if (idx >= content.carouselImageNames.length - 1) {
-      return 0;
-    }
-    return idx + 1;
+  const [arrowClicked, setArrowClicked] = useState<boolean>(false);
+  const [currentSlide, setCurrentSlide] = React.useState(0);
+
+  const slideNext = (e: any) => {
+    setCurrentSlide((prev) => {
+      setArrowClicked(true);
+      return prev + 1 === content.carouselImageNames.length ? 0 : currentSlide + 1;
+    });
+    
   };
-
-  const [imageState, setImageState] = useState({
-    index: 0,
-    next: getNextIndex(0),
-    move: true,
-  });
-
-  const imageStateRef: any = useRef<HTMLDivElement>(null);
-  imageStateRef.current = imageState;
-
-  const setIndexes = (idx: number) => {
-    setImageState((prevState) => {
-      return {
-        ...prevState,
-        index: idx,
-        next: getNextIndex(idx),
-      };
+  const slidePrev = (e: any) => {
+    console.log('l clicked')
+    setCurrentSlide((prev) => {
+      setArrowClicked(true);
+      return prev === 0 ? content.carouselImageNames.length - 1 : currentSlide - 1;
     });
   };
-  const [arrowClicked, setArrowClicked] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (imageState.move && !arrowClicked) {
-      const interval = setInterval(() => {
-        // on
-        setImageState((prev) => {
-          return {
-            ...prev,
-            move: true,
-          };
-        });
-
-        // off
-        setTimeout(() => {
-          setImageState((prev) => {
-            return {
-              ...prev,
-              move: false,
-            };
-          });
-
-          setIndexes(getNextIndex(imageStateRef.current.index));
-        }, 600); // same delay as in the css transition here
-      }, 2000);
-
-      return () => clearInterval(interval);
-    }
-    return () => clearInterval("");
+  React.useEffect(() => {
+    if(!arrowClicked){
+    const intervalId = setInterval(() => {
+      setCurrentSlide((prev) => {
+        return prev + 1 === content.carouselImageNames.length ? 0 : prev + 1;
+      });
+    }, 2000);
+    return () => {
+      clearInterval(intervalId);
+    }}
   }, [arrowClicked]);
-
-  const move = imageState.move ? "move" : "";
-  const MAX_IMAGES = content.carouselImageNames.length - 1;
-  const handleMoveLeft = () => {
-    const indexToMove =
-      imageState.index - 1 >= 0 ? imageState.index - 1 : MAX_IMAGES;
-    setIndexes(indexToMove);
-    setArrowClicked(true);
-  };
-  const handleMoveRight = () => {
-    const indexToMove =
-      imageState.index + 1 <= MAX_IMAGES ? imageState.index + 1 : 0;
-    setIndexes(indexToMove);
-    setArrowClicked(true);
-  };
-  // const [index, setIndex] = React.useState(0);
-  // const timeoutRef: any = React.useRef(null);
-
-  // function resetTimeout() {
-  //   if (timeoutRef.current) {
-  //     clearTimeout(timeoutRef.current);
-  //   }
-  // }
-
-  // React.useEffect(() => {
-  //   resetTimeout();
-  //   timeoutRef.current = setTimeout(
-  //     () =>
-  //       setIndex((prevIndex) =>
-  //         prevIndex === content.carouselImageNames.length - 1
-  //           ? 0
-  //           : prevIndex + 1
-  //       ),
-  //     2500
-  //   );
-
-  //   return () => {
-  //     resetTimeout();
-  //   };
-  // }, [index]);
-  // const [sliderInitialized, setSliderInitialized] = useState<boolean>(false);
-  // useEffect(() => {
-  //   if (!sliderInitialized) {
-  //     setTimeout(() => {
-  //       setSliderInitialized(true);
-  //     }, 2800);
-  //   }
-  // }, [sliderInitialized]);
-
   return (
     <div className="p-2">
-      <ContainerX className="mt-32 justify-center md:!items-center lg:mt-0 max-h-fit ">
+      <ContainerX className="mt-32 justify-center md:!items-center lg:mt-0 max-h-fit relative z-30 ">
         <div className="mx-auto  text-center lg:text-left w-full xl:w-[50%] xl:ml-0 xl:pl-4">
           <p className="font-bold font-sans text-3.25xl md:text-4xl leading-10 md:leading-snug mb-7  mx-auto text-purple-888 font-bold ">
             Reach voters where they are, before and on Election Day!
@@ -372,35 +284,18 @@ const Join = (): JSX.Element => {
           </div>
         </div>
         <Image className="xxs:p-2 xsm:p-2 sm:p-20 md:p-20 lg:p-0 xl:p-0 ">
-          {/* <ContributeImage /> */}
-          {/* {content.carouselImageNames[index]} */}
           <ImageC>
             <BlockNFT>
               <div className="mask">
-                <LeftArrow onClick={handleMoveLeft} />
-                <AnimatedImage
-                  innerRef={imageStateRef}
-                  move={move}
-                  index={imageState.index}
-                  arrowUsed={arrowClicked}
-                />
-                {/* {!sliderInitialized && (
-                  <div
-                    className={`pic-wrapper`}
-                    style={{
-                      transition: "all 0.5s",
-                      transitionDelay: "300ms",
-                    }}
-                  >
-                    <div
-                      className={`current pic move`}
-                      style={index === 0 ? { left: "125%" } : { opacity: 0 }}
-                    >
-                      <img src={content.carouselImageNames[0]} alt="asd" />
-                    </div>
-                  </div>
-                )} */}
-                <RightArrow onClick={handleMoveRight} />
+                <LeftArrow className="hover:cursor-pointer" onClick={slidePrev} />
+                <Truck className="img-wrapper">
+                  <img
+                    src={content.carouselImageNames[currentSlide]}
+                    alt=""
+                    className="ph"
+                  />
+                </Truck>
+                <RightArrow className="hover:cursor-pointer" onClick={slideNext} />
               </div>
             </BlockNFT>
           </ImageC>
@@ -411,59 +306,59 @@ const Join = (): JSX.Element => {
       <ContainerX className="mt-4 min-h-fit lg:mt-32 pb-6 lg:mb-20 xl:pt-0 relative">
         <div className="flex items-center lg:justify-between flex-col lg:flex-row w-full relative">
           <div className="im mt-12 pl-0 w-full max-w-[100%] lg:max-w-[55%]">
-          <AnnonBack />
-          <FormCard className="  w-full text-left">
-            <FormCardBody className="pt-0">
-              <div className="flex flex-col xsm:flex-row xsm:gap-5">
-                <div className="flex-1">
-                  <Label>First Name</Label>
-                  <TextInput placeholder="Abe" />
+            <AnnonBack />
+            <FormCard className="  w-full text-left">
+              <FormCardBody className="pt-0">
+                <div className="flex flex-col xsm:flex-row xsm:gap-5">
+                  <div className="flex-1">
+                    <Label>First Name</Label>
+                    <TextInput placeholder="Abe" />
+                  </div>
+                  <div className="flex-1">
+                    <Label>Last Name</Label>
+                    <TextInput placeholder="Lincoln" />
+                  </div>
                 </div>
-                <div className="flex-1">
-                  <Label>Last Name</Label>
-                  <TextInput placeholder="Lincoln" />
+                <div className="flex flex-col xsm:flex-row xsm:gap-5">
+                  <div className="flex-1">
+                    <Label>Work Email</Label>
+                    <TextInput placeholder="Abe.Lincoln@us.gov" />
+                  </div>
+                  <div className="flex-1">
+                    <Label>Phone</Label>
+                    <TextInput placeholder="+1(123) 123-1234" />
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-col xsm:flex-row xsm:gap-5">
-                <div className="flex-1">
-                  <Label>Work Email</Label>
-                  <TextInput placeholder="Abe.Lincoln@us.gov" />
+                <div className="flex flex-col xsm:flex-row xsm:gap-5">
+                  <div className="flex-1">
+                    <Label>Organization Name</Label>
+                    <TextInput placeholder="US GOV" />
+                  </div>
                 </div>
-                <div className="flex-1">
-                  <Label>Phone</Label>
-                  <TextInput placeholder="+1(123) 123-1234" />
+                <div className="flex flex-col xsm:flex-row xsm:gap-5">
+                  <div className="flex-1">
+                    <Label>Organization Type</Label>
+                    <TextInput placeholder="Government" />
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-col xsm:flex-row xsm:gap-5">
-                <div className="flex-1">
-                  <Label>Organization Name</Label>
-                  <TextInput placeholder="US GOV" />
+                <div className="flex flex-col xsm:flex-row xsm:gap-5">
+                  <div className="flex-1">
+                    <Label>Website</Label>
+                    <TextInput placeholder="us.gov" />
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-col xsm:flex-row xsm:gap-5">
-                <div className="flex-1">
-                  <Label>Organization Type</Label>
-                  <TextInput placeholder="Government" />
+                <div className="flex flex-col xsm:flex-row xsm:gap-5">
+                  <div className="flex-1">
+                    <Label>Why are you interested in Engage?</Label>
+                    <TextInput placeholder="..." />
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-col xsm:flex-row xsm:gap-5">
-                <div className="flex-1">
-                  <Label>Website</Label>
-                  <TextInput placeholder="us.gov" />
-                </div>
-              </div>
-              <div className="flex flex-col xsm:flex-row xsm:gap-5">
-                <div className="flex-1">
-                  <Label>Why are you interested in Engage?</Label>
-                  <TextInput placeholder="..." />
-                </div>
-              </div>
 
-              <Button className="mt-5 !px-14" $secondary>
-                Book Demo
-              </Button>
-            </FormCardBody>
-          </FormCard>
+                <Button className="mt-5 !px-14" $secondary>
+                  Book Demo
+                </Button>
+              </FormCardBody>
+            </FormCard>
           </div>
           <div className="m-auto lg:pl-10 md:mt-12 md:pt-7 xsm:mt-10 lg:text-left z-10">
             <h4 className=" text-left pl-4 mt-4 mb-2 md:my-0 text-3.5xl md:text-[32px] text-purple-888 font-bold leading-[1.23em]">
@@ -471,17 +366,16 @@ const Join = (): JSX.Element => {
             </h4>
             <Paragraph className="text-left font-medium font-sans mt-4 pl-4   md:text-lg leading-relaxed sm:leading-loose">
               <List>
-              <li>Grow your contact list</li>
-              <li>Keep your voters informed and ready to vote</li>
-              <li>Motivate voters to invite their network to support you</li>
-              <li>Use data to target potential voters</li>
-              <li>Turn supporters into donors</li>
+                <li>Grow your contact list</li>
+                <li>Keep your voters informed and ready to vote</li>
+                <li>Motivate voters to invite their network to support you</li>
+                <li>Use data to target potential voters</li>
+                <li>Turn supporters into donors</li>
               </List>
             </Paragraph>
           </div>
         </div>
       </ContainerX>
-      {/* </BgFormContainer> */}
     </div>
   );
 };
