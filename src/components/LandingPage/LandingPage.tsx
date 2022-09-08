0c036d90e3326f867e53016ff6bbfecfe8244786
Khan047 <tajzz.tk@gmail.com>
2022-09-08 08:53:49 +0530

    :heavy_check_mark:page redo

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,8 +1,8 @@
-import { Card, CardBody } from '@material-tailwind/react';
-import styled from 'styled-components';
-import tw from 'tailwind-styled-components';
+import { Card, CardBody } from "@material-tailwind/react";
+import styled from "styled-components";
+import tw from "tailwind-styled-components";
 
-import Container from '../../layout/Container';
+import Container from "../../layout/Container";
 import {
   downloadApple,
   downloadGoogle,
@@ -25,17 +25,11 @@ import {
   stroke3,
   stroke4,
   stroke6,
-} from '../../utils/imagePaths';
+  sideshape6,
+} from "../../utils/imagePaths";
 
-import Button from '../UI/Button/Button';
-import TextInput from '../UI/Input/TextInput';
-
-// const TitleWrapper = tw.div`
-//   mb-8
-//   md:my-20
-//   lg:my-auto
-//   w-140
-// `;
+import Button from "../UI/Button/Button";
+import TextInput from "../UI/Input/TextInput";
 
 const Title = tw.h1`
   text-3xl
@@ -51,21 +45,6 @@ const Title = tw.h1`
   md:max-w-full
 `;
 
-// const Subtitle = tw.div`
-//   text-3xl
-//   xsm:text-3.25xl
-//   md:text-4xl
-//   lg:text-4.5xl
-//   xsm:leading-10
-//   md:leading-11
-//   lg:leading-12
-//   text-purple-800
-//   mb-7
-//   max-w-sm
-//   sm:max-w-md
-//   md:max-w-full
-// `;
-
 const Paragraph = tw.div`
   xsm:text-lg
   md:text-lg
@@ -76,20 +55,6 @@ const Paragraph = tw.div`
   sm:max-w-md
   md:max-w-full
 `;
-
-// const Disclaimer = tw.div`
-//   flex
-//   w-full
-//   justify-center
-//   text-center
-
-//   px-8
-//   xsm:px-10
-//   pb-8
-
-//   text-sm
-//   text-red-500
-// `;
 
 const LandingContainer = styled.div`
   background-image: url(${sideshape1}), url(${sideshape2}), url(${sideshape3}),
@@ -132,7 +97,8 @@ const Image = styled.div`
   background-position: 41% 54%, 19% 51%, 28% 59%;
 
   @media only screen and (max-width: 1024px) {
-    background-size: 39% 91%, 71% 139%, 79% 135%;
+    background-size: 12em 24em, 71% 139%, 79% 135%;
+    // background-size: 39% 91%, 71% 139%, 79% 135%;
     background-position: 49% 93%, 41% 33%, 48% 36%;
   }
 
@@ -241,17 +207,15 @@ const Image4 = styled.div`
 const Image6 = styled.div`
   background-image: url(${mainshape6}), url(${stroke6});
   background-repeat: no-repeat;
-  // background-size: 86% 99%, 102% 90%;
-  // background-position: 63% 72%, 3% 105%;
-  background-size: 101% 100%,104% 95%;
-  background-position: 2em 0em,3% 105%;
+  background-size: 101% 100%, 104% 95%;
+  background-position: 2em 0em, 3% 105%;
   padding: 3em;
   margin: -3em;
   @media only screen and (max-width: 1024px) {
     // background-size: 86% 99%, 102% 90%;
     // background-position: 63% 72%, 3% 105%;
-    background-size: 101% 100%,104% 95%;
-    background-position: 2em 0em,3% 105%;
+    background-size: 101% 100%, 104% 95%;
+    background-position: 2em 0em, 3% 105%;
     padding: 4em;
     margin: -4em;
   }
@@ -270,21 +234,6 @@ const Image6 = styled.div`
     background-position: 17% 69%, -145% 69%;
   }
 `;
-
-// const FormBgImage1 = styled.img`
-//   height: 516px;
-//   width: 534px;
-//   position: absolute;
-//   top: -80px;
-//   left: 7px;
-// `;
-// const FormBgImage2 = styled.img`
-//   height: 516px;
-//   width: 534px;
-//   position: absolute;
-//   top: -140px;
-//   left: 7px;
-// `;
 
 const ContributeImage = tw.div`
   background-repeat: no-repeat;
@@ -310,23 +259,279 @@ const Label = tw.div`
   mt-6
   mb-1
 `;
+// new start
+const ContainerNew = tw.div`
+container
+text-center
+mx-auto
+mt-32
+px-0
+md:px-10
+lg:px-24
+xl:px-14
+2xl:px-36
+md:text-left
+flex-none
+md:flex
+md:items-center
+min-h-[55vh]
+z-20
+relative
+`;
+const ContainerFluid = tw.div`
+w-full
+relative
+`;
+// new end
+const ImageWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  .image-inner-wrapper {
+    max-width: 500px;
+    max-height: 500px;
+    margin: 0 auto;
+    margin-right: 0;
+  }
+  img.stroke_layer {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+  }
+  img.fill_layer {
+    width: 90%;
+    height: 100%;
+    position: absolute;
+  }
+  .phone {
+    width: 100%;
+    height: 100%;
+    max-width: 250px;
+    max-height: 500px;
+    margin: 0 auto;
+    z-index: 10;
+    position: relative;
+  }
+  @media only screen and (max-width: 1024px) {
+    .phone.phone-sec-2 {
+      max-height: 375px;
+      max-width: 200px;
+    }
+  }
+  @media only screen and (max-width: 920px) {
+    .phone {
+      max-width: 200px;
+    }
+    .phone.phone-sec-2 {
+      max-height: 316px;
+      max-width: 171px;
+    }
+  }
 
+  @media only screen and (max-width: 640px) {
+  }
+
+  @media only screen and (max-width: 420px) {
+  }
+`;
 const LandingPage = (): JSX.Element => {
-  // const imageStyleContribute = {
-  //   backgroundImage: `url(/assets/images/abe-lincoln-contribute.png)`,
-  // };
-  // const imageStyleDefi = {
-  //   backgroundImage: `url(/assets/images/defi-public.jpeg)`,
-  // };
-  // const downloadApple = {
-  //   backgroundImage: `url(/assets/images/download-apple.svg)`,
-  // };
-  // const imageStyleDonations = {
-  //   backgroundImage: `url(/assets/images/secure-donations.jpeg)`,
-  // };
-
   return (
-    <LandingContainer>
+    <ContainerFluid>
+      <ContainerFluid>
+        <img
+          className="absolute bottom-1/2 md:bottom-[-40%] left-[-2%]"
+          src={sideshape1}
+          alt=""
+        />
+        {/* <img className="absolute right-0" src={sideshape2} alt="" />
+      <img className="absolute left-0" src={sideshape3} alt="" />
+      <img className="absolute right-0" src={sideshape4} alt="" />
+      <img className="absolute left-0" src={sideshape5} alt="" /> */}
+        {/* SECTION - 1 */}
+        <ContainerNew className="lg:items-start">
+          <div className="w-full px-4 mx-auto md:px-0 md:m-auto md:max-w-xl lg:text-left">
+            <Title className="text-left">
+              Democracy needs engaged citizens more than ever before. We’re here
+              to help.
+            </Title>
+            <Paragraph className="text-left">
+              Participating in democracy has never been more important, but it
+              seems harder than ever to keep up with everything. Designed by
+              policy experts and software engineers who care deeply about
+              democracy and civic health, the Engage app gives you all the
+              election information you need in five minutes or less, and helps
+              you get your friends and family to the polls as well!
+            </Paragraph>
+            <div className=" flex flex-col md:flex-row">
+              <img
+                className="ml-0 h-12 md:mr-3 md:ml-0 w-fit md:w-auto"
+                src={downloadApple}
+                alt="store_icon"
+              />
+              <img
+                className="ml-0 h-12 mt-4 md:mt-0 md:m-0 w-fit md:w-auto"
+                src={downloadGoogle}
+                alt="store_icon"
+              />
+            </div>
+          </div>
+          <ImageWrapper className="mt-16 px-4 md:px-0 md:mt-0 h-full relative">
+            <div className="image-inner-wrapper relative">
+              <img src={stroke1} alt="" className="stroke_layer " />
+              <img src={mainshape1} alt="" className="fill_layer " />
+              <img src={phone1} alt="" className="phone" />
+            </div>
+          </ImageWrapper>
+        </ContainerNew>
+      </ContainerFluid>
+      {/* SECTION - 2 */}
+      <ContainerFluid>
+        <img className="absolute right-0" src={sideshape2} alt="" />
+        <ContainerNew className="mt-24 md:mt-64  flex flex-col-reverse md:flex-row justify-around">
+          <ImageWrapper className="mt-16 px-4 md:px-0 md:pr-16 md:mt-0 h-full relative">
+            <div className="image-inner-wrapper relative  w-m-[500px]">
+              <img
+                src={stroke2}
+                alt=""
+                className="stroke_layer_2 absolute w-fit top-[15%]"
+              />
+              <img
+                src={mainshape2}
+                alt=""
+                className="fill_layer_2 absolute top-[10%] w-fit"
+              />
+              <img
+                src={phone2}
+                alt=""
+                className="phone phone-sec-2 top-0 absolute"
+              />
+            </div>
+          </ImageWrapper>
+          <div className="m-auto lg:text-left w-full">
+            <Title className="!m-0">
+              Find your district, <br></br> candidates & voting<br></br>{" "}
+              location
+            </Title>
+          </div>
+        </ContainerNew>
+      </ContainerFluid>
+      {/* SECTION - 3 */}
+      <ContainerFluid>
+        <img className="absolute left-0" src={sideshape3} alt="" />
+        <ContainerNew className="mt-60 md:mt-32 items-center  flex flex-col md:flex-row justify-around">
+          <div className="m-auto md:m-auto  w-full px-4 md:px-0 md:pr-16">
+            <Title className="m-0 text-center md:text-right">
+              Elections synced to your calendar so you never miss a vote!
+            </Title>
+          </div>
+          <ImageWrapper className="scale-[0.85] xsm:scale-1 mt-16  md:mt-0 h-full relative">
+            <div className="image-inner-wrapper relative  w-m-[500px]">
+              <img
+                src={stroke3}
+                alt=""
+                className="stroke_layer_2 absolute w-fit top-[15%]"
+              />
+              <img
+                src={mainshape3}
+                alt=""
+                className="fill_layer_2 absolute top-[5%] w-fit"
+              />
+              <img src={phone3} alt="" className="phone top-0 absolute" />
+            </div>
+          </ImageWrapper>
+        </ContainerNew>
+      </ContainerFluid>
+      {/* SECTION - 4 */}
+      <ContainerFluid>
+      <img className="absolute right-0" src={sideshape4} alt="" />
+        <ContainerNew className=" md:mt-64  flex flex-col-reverse md:flex-row justify-around">
+          <ImageWrapper className="mt-16 px-4 md:px-0 md:pr-16 md:mt-0 h-full relative">
+            <div className="image-inner-wrapper relative  w-m-[500px]">
+              <img
+                src={stroke4}
+                alt=""
+                className="stroke_layer_2 absolute w-fit top-[15%]"
+              />
+              <img
+                src={mainshape4}
+                alt=""
+                className="fill_layer_2 absolute top-[10%] w-fit"
+              />
+              <img
+                src={phone4}
+                alt=""
+                className="phone phone-sec-2 top-0 absolute"
+              />
+            </div>
+          </ImageWrapper>
+          <div className="m-auto lg:text-left w-full">
+            <Title className="m-0">
+              Be a more active citizen, right from your smartphone!
+            </Title>
+          </div>
+        </ContainerNew>
+      </ContainerFluid>
+      {/* SECTION - 5 */}
+      <ContainerFluid>
+        <img className="absolute left-0" src={sideshape5} alt="" />
+        <ContainerNew className="mt-64 md:mt-24  flex flex-col md:flex-row justify-around">
+          <div className="m-auto  w-full px-4 md:px-0 md:pr-16">
+            <Title className="m-0 text-center md:text-right">
+            We make it easy to mobilize your friends and family
+            </Title>
+          </div>
+          <ImageWrapper className="scale-[0.85] xsm:scale-1 mt-16  md:mt-0 h-full relative">
+            <div className="image-inner-wrapper relative  w-m-[500px]">
+              <img
+                src={stroke3}
+                alt=""
+                className="stroke_layer_2 absolute w-fit top-0 rotate-[177deg]"
+              />
+              <img
+                src={mainshape3}
+                alt=""
+                className="fill_layer_2 absolute top-[5%] w-fit"
+              />
+              <img src={phone3} alt="" className="phone top-0 absolute" />
+            </div>
+          </ImageWrapper>
+        </ContainerNew>
+      </ContainerFluid>
+      {/* SECTION - 5 */}
+      <ContainerFluid>
+        <img className="absolute right-0" src={sideshape6} alt="" /> 
+        <ContainerNew className="mt-24 md:mt-24  flex flex-col-reverse md:flex-row justify-around mb-32">
+          <Image6 className="xxs:py-24 xxs:px-6 xsm:p-20 sm:p-20 md:p-20 lg:p-0 xl:p-0">
+            <Card className="mx-auto xxs:w-64 xsm:w-72 sm:w-96 md:w-96 rounded-[30px]" style={{boxShadow:" 0 0 34px 0 rgba(184,167,240,0.47)"}}>
+              <CardBody>
+                <Label>Name</Label>
+                <TextInput placeholder="Abe" />
+                <Label>Email</Label>
+                <TextInput placeholder="Abe.Lincoln@us.gov" />
+                <Button className="mt-5 !px-14" $secondary>
+                  Sign Up
+                </Button>
+              </CardBody>
+            </Card>
+          </Image6>
+          {/* <div className="m-auto lg:text-left w-full">
+            <Title className="m-auto max-w-xl flex-1">
+              Be a more active citizen, right from your smartphone!
+            </Title>
+            <Paragraph>
+              Please sign up here for early access to our app, and join the
+              movement for a better democracy today!
+            </Paragraph>
+          </div> */}
+           <div className="m-auto max-w-xl flex-1 lg:text-left md:pl-28">
+            <Title>Stay informed</Title>
+            <Paragraph>
+              Please sign up here for early access to our app, and join the
+              movement for a better democracy today!
+            </Paragraph>
+          </div> 
+        </ContainerNew>
+      </ContainerFluid>
+      {/* <LandingContainer>
       <Container className="flex min-h-screen xsm:px-1 xxs:px-1">
         <div className="m-auto flex flex-col justify-around gap-0 lg:flex-row lg:gap-10">
           <div className="w-full px-4 mx-auto md:px-0 md:m-auto md:max-w-xl lg:text-left">
@@ -425,8 +630,9 @@ const LandingPage = (): JSX.Element => {
             </Paragraph>
           </div>
         </div>
-      </Container>
-    </LandingContainer>
+      </Container> 
+    </LandingContainer>*/}
+    </ContainerFluid>
   );
 };
 
